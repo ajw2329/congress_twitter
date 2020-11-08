7c57fed7612c53a5baab3ad6a0dd3ab28d2be383
ajw2329 <ajw2329@gmail.com>
2020-11-07 23:12:10 -0800

    Add working example of scraping with snscrape

---
+++ scripts/scraping_tweets.py
@@ -0,0 +1,16 @@
+
+
+import snscrape.modules.twitter as sntwitter
+import csv
+maxTweets = 3000
+
+csvFile = open('place_result.csv', 'a', newline='', encoding='utf8')
+
+csvWriter = csv.writer(csvFile)
+csvWriter.writerow(['id','date','tweet',])
+
+for i,tweet in enumerate(sntwitter.TwitterSearchScraper('from:@SpeakerPelosi + since:2020-11-01 until:2020-11-05-filter:links -filter:replies').get_items()):
+	if i > maxTweets :
+		break
+csvWriter.writerow([tweet.id, tweet.date, tweet.content])
+csvFile.close()
