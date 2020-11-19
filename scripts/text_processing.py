12ca278befa83c9519d3cb5c6a12d049c4d66551
ajw2329 <ajw2329@gmail.com>
2020-11-18 23:01:17 -0800

    Begin experiments with simple models

--- scripts/text_processing.py
+++ scripts/text_processing.py
@@ -1,7 +1,29 @@
 import spacy
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
 
 # Load the en_core_web_sm model
-nlp = spacy.load('en_core_web_sm')
+nlp = spacy.load('en_core_web_lg')
+
+stopwords = nlp.Defaults.stop_words
+
+all_tweets["tweet_lemmas"] = all_tweets["tweet_content"].apply(preprocess)
+
+train_X, test_X, train_y, test_y = train_test_split(all_tweets['tweet_lemmas'], pd.get_dummies(all_tweets['party'])["D"], test_size=0.3, random_state=42, stratify=all_tweets['party'])
+
+# Generating ngrams
+vectorizer = CountVectorizer(ngram_range = (1,3))
+train_X = vectorizer.fit_transform(train_X)
+test_X = vectorizer.transform(test_X)
+
+# Fit classifier
+clf = MultinomialNB()
+clf.fit(train_X, train_y)
+
+accuracy = clf.score(test_X, test_y)
+print("The accuracy of the classifier on the test set is %.3f" % accuracy)
 
 
 def lemmatize(text, nlp_model):
@@ -59,3 +81,101 @@ def find_persons(text):
   # Return persons
   return persons
 
+
+
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_matrix = vectorizer.fit_transform(corpus)
+
+# Print the shape of bow_matrix
+print(bow_matrix.shape)
+
+
+
+
+# Create CountVectorizer object
+vectorizer = CountVectorizer()
+
+# Generate matrix of word vectors
+bow_matrix = vectorizer.fit_transform(corpus)
+
+# Convert bow_matrix into a DataFrame
+bow_df = pd.DataFrame(bow_matrix.toarray())
+
+# Map the column names to vocabulary 
+bow_df.columns = vectorizer.get_feature_names()
+
+# Print bow_df
+print(bow_df)
+
+
+
+
+# Import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Create a CountVectorizer object
+vectorizer = CountVectorizer(lowercase=True, stop_words="english")
+
+# Fit and transform X_train
+X_train_bow = vectorizer.fit_transform(X_train)
+
+# Transform X_test
+X_test_bow = vectorizer.transform(X_test)
+
+# Print shape of X_train_bow and X_test_bow
+print(X_train_bow.shape)
+print(X_test_bow.shape)
+
+
+
+
+# Create a MultinomialNB object
+clf = MultinomialNB()
+
+# Fit the classifier
+clf.fit(X_train_bow, y_train)
+
+# Measure the accuracy
+accuracy = clf.score(X_test_bow, y_test)
+print("The accuracy of the classifier on the test set is %.3f" % accuracy)
+
+# Predict the sentiment of a negative review
+review = "The movie was terrible. The music was underwhelming and the acting mediocre."
+prediction = clf.predict(vectorizer.transform([review]))[0]
+print("The sentiment predicted by the classifier is %i" % (prediction))
+
+
+start_time = time.time()
+# Splitting the data into training and test sets
+train_X, test_X, train_y, test_y = train_test_split(df['review'], df['sentiment'], test_size=0.5, random_state=42, stratify=df['sentiment'])
+
+# Generating ngrams
+vectorizer = CountVectorizer(ngram_range = (1,3))
+train_X = vectorizer.fit_transform(train_X)
+test_X = vectorizer.transform(test_X)
+
+# Fit classifier
+clf = MultinomialNB()
+clf.fit(train_X, train_y)
+
+# Print accuracy, time and number of dimensions
+print("The program took %.3f seconds to complete. The accuracy on the test set is %.2f. The ngram representation had %i features." % (time.time() - start_time, clf.score(test_X, test_y), train_X.shape[1]))
+
+# Import TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# Create TfidfVectorizer object
+vectorizer = TfidfVectorizer()
+
+# Generate matrix of word vectors
+tfidf_matrix = vectorizer.fit_transform(ted)
+
+# Print the shape of tfidf_matrix
+print(tfidf_matrix.shape)
